e5a72817e03346f19f9b1b3f93af1933b671c5c2
Zach T <ztrokey@gmail.com>
2021-07-17 14:13:45 -0600

    add invoice items

---
+++ app/models/invoice_item.rb
@@ -0,0 +1,8 @@
+class InvoiceItem < ApplicationRecord
+  validates_presence_of :invoice_id,
+                        :item_id,
+                        :quantity,
+                        :unit_price
+  belongs_to :invoice
+  belongs_to :item
+end
