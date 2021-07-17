9ec5aea269b10b4b4c1a30b7e2ee92262555ccbf
Zach T <ztrokey@gmail.com>
2021-07-17 14:15:59 -0600

    add invoices

---
+++ app/models/invoice.rb
@@ -0,0 +1,12 @@
+class Invoice < ApplicationRecord
+  validates_presence_of :status,
+                        :customer_id
+
+  belongs_to :customer
+  belongs_to :merchant
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+
+  enum status: [:cancelled, :in_progress, :complete]
+end
