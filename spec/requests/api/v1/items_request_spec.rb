a981a15c52f5af7a84d3f72b9f0e91afc63526a6
Zach T <ztrokey@gmail.com>
2021-07-19 17:00:18 -0600

    add test for item index

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -47,4 +47,47 @@ RSpec.describe 'Item requests' do
       expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
+
+  it 'returns all items 20 at a time' do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+
+    merchant1_items = create_list(:item, 25, merchant_id: merchant1.id)
+    merchant2_items = create_list(:item, 35, merchant_id: merchant2.id)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    item_data = JSON.parse(response.body, symbolize_names: true)
+    items = item_data[:data]
+
+    expect(item_data).to be_a(Hash)
+    expect(item_data).to have_key(:data)
+    expect(item_data[:data]).to be_an(Array)
+    expect(items.count).to eq(20)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
 end
+# get all items, a maximum of 20 at a time
+# get one item
+# create an item
+# edit an item
+# delete an item
+# get the merchant data for a given item ID
