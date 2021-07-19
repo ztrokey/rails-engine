b37c50cddbb927d7c9923d5d621c4b90726fbc68
Zach T <ztrokey@gmail.com>
2021-07-19 16:59:57 -0600

    update index method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Merchant.find(params[:merchant_id]).items
-    render json: ItemSerializer.new(items).serializable_hash.to_json
+    if params[:merchant_id]
+      items = Merchant.find(params[:merchant_id]).items
+      render json: ItemSerializer.new(items).serializable_hash.to_json
+    elsif
+      page = [params.fetch(:page, 1).to_i, 1].max
+      per_page = params.fetch(:per_page, 20).to_i
+      items = Item.limit(per_page).offset((page - 1) * per_page)
+      render json: ItemSerializer.new(items).serializable_hash.to_json
+      # require 'pry'; binding.pry
+    end
   end
 
-  def show
-    item = item.find(params[:id])
-    render json: item
-  end
+  # def show
+  #   item = item.find(params[:id])
+  #   render json: item
+  # end
 end
