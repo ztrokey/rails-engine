e829aef2f0647dcfcd683564809f75df8444b8fb
Zach T <ztrokey@gmail.com>
2021-07-19 15:19:08 -0600

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :invoices
-  resources :customers
-  resources :invoice_items
-  resources :items
-  resources :merchants
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index]
+      end
+    end
+  end
+  # resources :transactions
+  # resources :invoices
+  # resources :customers
+  # resources :invoice_items
+  # resources :items
 end
