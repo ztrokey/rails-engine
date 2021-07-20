7e9dbbe12a44ed376ca04951cdbf58fb4d7d0697
Zach T <ztrokey@gmail.com>
2021-07-19 21:22:00 -0600

    add items routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
       end
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        get '/merchant', to: 'items#show'
+      end
     end
   end
   # resources :transactions
   # resources :invoices
   # resources :customers
   # resources :invoice_items
-
 end
