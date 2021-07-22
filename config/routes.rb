d07e4b13f9eb11c064aecf09cdc4706e3e84818d
Zach T <ztrokey@gmail.com>
2021-07-22 09:10:54 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,20 @@ Rails.application.routes.draw do
         resources :find, controller: :search, only: :index
       end
 
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
+      resources :merchants, only: %i[index show] do
+        resources :items, only: :index
       end
 
       namespace :items do
         resources :find_all, controller: :search, only: :index
       end
-      
-      resources :items, only: [:index, :show, :create, :update, :destroy] do
+
+      resources :items, only: %i[index show create update destroy] do
         get '/merchant', to: 'items#show'
+      end
+
+      namespace :revenue do
+        resources :merchants, only: :show
       end
     end
   end
