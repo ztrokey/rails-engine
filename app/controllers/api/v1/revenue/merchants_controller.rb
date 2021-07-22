39c24fbbb8dcd8d4f9fbd676e84a41b17653ecfe
Zach T <ztrokey@gmail.com>
2021-07-22 17:57:06 -0600

    add index method to merchants controller

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
-   def show
+  def index
+    merchants = Merchant.most_revenue(params[:quantity])
+    render json: MerchantMostRevenueSerializer.new(merchants).serializable_hash.to_json
+  end
+
+  def show
     merchant = Merchant.find(params[:id])
     render json: MerchantRevenueSerializer.new(merchant).serializable_hash.to_json
-    # require 'pry'; binding.pry
-   end
+  end
 end
