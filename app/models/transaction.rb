0bd493918ac5542c86f863b1b7fc7dc9ab0c49f4
Zach T <ztrokey@gmail.com>
2021-07-22 23:00:56 -0600

    remove unused enums

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,6 @@ class Transaction < ApplicationRecord
   validates_presence_of :invoice_id,
                         :credit_card_number,
                         :result
-  # enum result: [:failed, :success]
 
   belongs_to :invoice
 end
